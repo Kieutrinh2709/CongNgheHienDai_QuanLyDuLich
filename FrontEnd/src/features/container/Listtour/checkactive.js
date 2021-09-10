5a74fb83aba47d9b11f46b3fba1a199a2ee6d380
KieuTrinh2709 <1851050155trinh@ou.edu.vn>
2021-09-10 20:37:38 +0700

    remove frontend

--- FrontEnd/src/features/container/Listtour/checkactive.js
+++
@@ -1,8 +0,0 @@
-let actives = document.querySelectorAll('li');
-actives.forEach(active => {
-    active.addEventListener('click', function () {
-        console.log("ok");
-        actives.forEach(btn => btn.classList.remove('active'));
-        this.classList.add('active');
-    })
-})
