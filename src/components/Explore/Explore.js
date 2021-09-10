f1bbed561587255c725258068ff179a228181a81
KieuTrinh2709 <1851050155trinh@ou.edu.vn>
2021-09-10 21:06:02 +0700

    up

---
+++ src/components/Explore/Explore.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import '../../../node_modules/materialize-css/dist/css/materialize.min.css';
+import './Explore.css';
+
+class Explore extends Component {
+  render() {
+    return (
+      <section id="explore" className="section section-icons grey lighten-4 center">
+        <div className="container">
+          <div className="row">
+          <h4 className="center">
+              Khám phá cùng<span className="black-text darken-1"> Memories Tour</span></h4>
+            <div className="col s6 m3">
+              <div className="card-panel">
+                <span class= "card-panel-24">
+                  <img src = "https://storage.googleapis.com/tripi-assets/mytour/icons/icon_support_247.svg" width = "52" height = "52" alt = "logo_support_24/7"/>
+                </span>
+                <hr  width="30%" align="center" />
+                <h1>Hỗ trợ khách hàng 24/7</h1>
+                <h2>Chat là có, gọi là nghe, luôn chu đáo, tận tâm và tận tình.</h2>
+              </div>
+            </div>
+            <div className="col s6 m3">
+              <div className="card-panel">
+                <span class= "card-panel-price">
+                  <img src = "https://storage.googleapis.com/tripi-assets/mytour/icons/icon_best_price.svg" width = "52" height = "52" alt = "logo_promotion"/>
+                </span>
+                <hr  width="30%" align="center" />
+                <h1>Giá ưu đãi</h1>
+                <h2>Luôn có mức giá ưu đãi cho khách hàng.</h2>
+              </div>
+            </div>
+            <div className="col s6 m3">
+              <div className="card-panel">
+                <span class= "card-panel-payment">
+                  <img src = "https://storage.googleapis.com/tripi-assets/mytour/icons/icon_payment.svg" width = "52" height = "52" alt = "logo_payment"/>
+                </span>
+                <hr  width="30%" align="center" />
+                <h1>Thanh toán tiện lợi</h1>
+                <h2>An toàn, linh hoạt thông qua app Momo và ZaloPay.</h2>
+              </div>
+            </div>
+            <div className="col s6 m3">
+              <div className="card-panel">
+                <span class= "card-panel-hotel">
+                  <img src = "https://storage.googleapis.com/tripi-assets/mytour/icons/icon_total_hotel.svg" width = "52" height = "52" alt = "logo_hotel"/>
+                </span>
+                <hr  width="30%" align="center" />
+                <h1>Dịch vụ </h1>
+                <h2>Hàng nghìn khách sạn sẽ là nơi nghỉ chân thoải mái nhất cho các bạn.</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Explore;
